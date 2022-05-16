c6c9c10225e509591ec8326f17e67f8a2fac5648
timothy081200 <s11810140@student.unklab.ac.id>
2022-05-16 12:28:56 +0800

    update user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,5 +3,5 @@ const authController = require('../controller/auth')
 
 router.post('/register',authController.register)
 router.post('/login',authController.login)
-
+router.put('/:id',authController.updateDataUser)
 module.exports = router
